b7f3f67a41fa7f5a0256834168b68b4a8dd01fa0
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-01-10 22:39:27 +0100

    ft-users-del user account deactivated

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,13 +1,12 @@
 import { Router } from 'express';
 import { signup } from '../services/auth/users';
-import { getAllUsers, getUser } from '../services/users/users';
+import { getAllUsers, getUser, destroyerUser } from '../services/users/users';
 
 const users = Router();
 
 users.post('/auth/registeration', signup);
-users.get('/auth/getAllUser', getAllUsers )
-users.get('/auth/getUser/:id', getUser )
-
-
+users.get('/auth/getAllUser', getAllUsers);
+users.get('/auth/getUser/:id', getUser);
+users.delete('/auth/destroyUser/:id', destroyerUser);
 
 export default users;
--- src/services/users/users.ts
+++ src/services/users/users.ts
@@ -1,40 +1,64 @@
-import User from "../../models/users";
-import AppError from "../../errors/errors";
-import {Request, Response, NextFunction } from "express";
+import User from '../../models/users';
+import AppError from '../../errors/errors';
+import { Request, Response, NextFunction } from 'express';
 
+export const getAllUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const allUsers = await User.find()
+      .exec()
+      .catch((err) => {});
+    if (!allUsers) return next(new AppError('Unable to retreive users', 404));
+    return res.status(202).json({
+      success: true,
+      message: 'All users has been retrieved',
+      data: allUsers,
+    });
+  } catch (err) {
+    console.log(err);
+    return next(new AppError('Unable to retreive users', 502));
+  }
+};
 
+export const getUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+  try {
+    const getThisUser = await User.findOne({ _id: id })
+      .exec()
+      .catch((err) => {});
+    if (!getThisUser)
+      return next(new AppError('Unable to retrieve this User', 404));
+    return res.status(200).json({
+      success: true,
+      message: 'All users has been retrieved',
+      data: getThisUser,
+    });
+  } catch (err) {
+    console.log(err);
+    return next(new AppError(`Unable to retreive this user ${err}`, 502));
+  }
+};
 
+export const destroyerUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+  const removeUser = await User.findOneAndDelete({ _id: id });
+  if (!removeUser)
+    return next(new AppError('Unable to perform this function', 404));
 
-export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const allUsers = await User.find().exec().catch((err) => {})
-        if(!allUsers) return next(new AppError('Unable to retreive users', 404))
-        return res.status(202).json({
-            success: true,
-            message: 'All users has been retrieved',
-            data: allUsers
-        })
-    } catch (err) {
-        console.log(err)
-        return next(new AppError('Unable to retreive users', 502))
-    }
-}
-
-export const getUser = async (req: Request, res: Response, next: NextFunction) => {
-
-    const { id } = req.params
-    try {
-        const getThisUser = await User.findOne({_id: id}).exec().catch((err) => {})
-        if (!getThisUser) return next(new AppError('Unable to retrieve this User', 404))
-        return res.status(200).json({
-            success: true,
-            message: 'All users has been retrieved',
-            data: getThisUser
-        })
-    } catch (err) {
-        console.log(err)
-        return next(new AppError(`Unable to retreive this user ${err}`, 502))
-        
-    }
-}
-
+  return res.status(200).json({
+    success: true,
+    message: `user with this id ${id} has been deleted`,
+    data: removeUser,
+  });
+};
