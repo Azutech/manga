d8c56d205201f2f18cb9b825b858eb36c43b3588
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-01-18 14:53:18 +0100

    ft-user-logout logout feature imnplemented

--- src/services/auth/users.ts
+++ src/services/auth/users.ts
@@ -138,6 +138,7 @@ export const logout = async (req: Request, res: Response) => {
         status: 'error',
       });
     }
+    await user.save()
     return res.status(200).json({ message: 'User logged out successfully' });
   } catch (err) {
     console.log(err);
