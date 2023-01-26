7dff4144b1a9058c4fe954d9e84dea8af47a22da
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-01-26 20:08:43 +0100

    products
    major routes set

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import users from './user';
 import auth from './auth';
 import { categor } from './products/cartegory';
+import {products} from '../routes/products/products'
 
 const routes = Router();
 
 routes.use('/users', users);
 routes.use('/users', auth);
-routes.use('products', categor);
+routes.use('/products', categor);
+routes.use('/products', products);
 
 export default routes;
